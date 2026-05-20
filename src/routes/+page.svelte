0a57af928f14b978cb0282fd18e1b830c46d8f87
Miguel Cliver <miguelcliver@gmail.com>
2026-05-20 15:47:25 -0400

    cambio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <section>
-    <p>Visual storytelle</p>
+    <p>Editor de video profesional</p>
     <div>
-        <p>Established in los angeles
-        </p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, possimus recusandae? Maxime ab necessitatibus, veritatis qui distinctio sapiente praesentium nemo accusantium asperiores, magnam consequatur quidem perspiciatis sunt commodi voluptate consectetur.</p>
+        <p>Michael Montañez</p>
+        <p>Hola, soy Michael. Me dedico a la edición de videos con un objetivo claro: ayudar a empresas y profesionales a comunicar sus ideas de forma clara y atractiva. Si buscas un trabajo de calidad, puntualidad y un resultado adaptado a tus necesidades, estaré encantado de que trabajemos juntos en tu próximo proyecto.</p>
     </div>
+<img src="/unnamed.png" alt="">
 </section>

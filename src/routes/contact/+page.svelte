d936b4beb8e898e921e35de79bde7260a519c8cd
Miguel Cliver <miguelcliver@gmail.com>
2026-05-21 12:05:48 -0400

    cambio

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,4 +1,3 @@
 <section>
     <p>let build something</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laborum id iste delectus in ullam, quae ipsa, consectetur dicta provident perspiciatis nisi rem animi quibusdam. Illo voluptas ducimus vel quo?</p>
 </section>

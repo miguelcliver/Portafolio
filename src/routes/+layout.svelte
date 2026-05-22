0943a9dde7b853b0afaa6b21d2f8d9768e1d0bd7
Miguel Cliver <miguelcliver@gmail.com>
2026-05-22 10:28:53 -0400

    div

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,37 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <nav>
-	<p>The frame</p>
-	<div>
-	<a href="/">menu</a>
-	<a href="/word">Work</a>
-	</div>
-<a href="/contact">
-<button>Contacto</button>
-</a>
+  <p>The frame</p>
+  <div>
+    <a href="/">menu</a>
+    <a href="/word">Work</a>
+  </div>
+  <a href="/contact">
+    <button>Contacto</button>
+  </a>
 </nav>
-
 
 {@render children()}
 
 <footer>
-	<div>
-		<img src="" alt="">
-		<p>Todos los derechos reservados</p>
-	</div>
-	<div>
-		<a href="https://www.instagram.com/?hl=es">instagram</a>
-		<a href="https://vimeo.com/es">vimeo</a>
-		<a href="https://ve.linkedin.com/">linkedin</a>
-	</div>
-	<div>
-		<p>estado disponible</p>
-		<p>work@thefame.pro</p>
-	</div>
+  <div>
+    <img src="" alt="" />
+    <p>Todos los derechos reservados</p>
+  </div>
+  <div>
+    <a href="https://www.instagram.com/?hl=es">instagram</a>
+    <a href="https://vimeo.com/es">vimeo</a>
+    <a href="https://ve.linkedin.com/">linkedin</a>
+  </div>
+  <div>
+    <p>estado disponible</p>
+    <p>work@thefame.pro</p>
+  </div>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
 <section>
-    <p>Editor de video profesional</p>
+  <p>Editor de video profesional</p>
+  <div>
+    <p>Michael Montañez</p>
+    <p>escrito de algo</p>
+  </div>
+  <img src="/unnamed.png" alt="" />
+</section>
+
+<section>
+  <div>
+    <p>Mi historia</p>
+  </div>
+  <div>
+    <p>texto1</p>
+    <p>texto2</p>
     <div>
-        <p>Michael Montañez</p>
-        <p>escrito de algo</p>
+      <div>
+        <p>textoverde1</p>
+        <p>textoblanco1</p>
+      </div>
+      <div>
+        <p>textoverde2</p>
+        <p>textoblanco2</p>
+      </div>
     </div>
-<img src="/unnamed.png" alt="">
+  </div>
 </section>

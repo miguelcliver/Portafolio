1a8e6ccf41cd4cc01e1b8d850f364a8eec3b9466
Miguel Cliver <miguelcliver@gmail.com>
2026-05-22 09:43:43 -0400

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,21 @@
 </a>
 </nav>
 
+
 {@render children()}
 
-footer
+<footer>
+	<div>
+		<img src="" alt="">
+		<p>Todos los derechos reservados</p>
+	</div>
+	<div>
+		<a href="https://www.instagram.com/?hl=es">instagram</a>
+		<a href="https://vimeo.com/es">vimeo</a>
+		<a href="https://ve.linkedin.com/">linkedin</a>
+	</div>
+	<div>
+		<p>estado disponible</p>
+		<p>work@thefame.pro</p>
+	</div>
+</footer>

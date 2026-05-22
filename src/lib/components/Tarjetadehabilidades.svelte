d8e4e43a9518cdff7b3483eb41aa52ad03bf78be
Miguel Cliver <miguelcliver@gmail.com>
2026-05-22 11:48:57 -0400

    edicion

---
+++ src/lib/components/Tarjetadehabilidades.svelte
@@ -0,0 +1,8 @@
+<script>
+    const {titulo,descripcion} = $props()
+</script>
+
+<div>
+    <p>{titulo}</p>
+    <p>{descripcion}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import Tarjetadehabilidades from "$lib/components/Tarjetadehabilidades.svelte";
+
+</script>
 <section>
   <p>Editor de video profesional</p>
   <div>
@@ -25,4 +29,18 @@
       </div>
     </div>
   </div>
+</section>
+
+<section>
+    <div>
+        <p>HABILIDADES</p>
+    </div>
+    <div>
+        <Tarjetadehabilidades titulo="Soporte tecnico" descripcion="servicio de asistencia que ayuda a los usuarios o empresas a resolver problemas con sus equipos, programas o sistemas tecnológicos" />
+        <Tarjetadehabilidades titulo="diseño sonoro" descripcion="El diseño sonoro es la disciplina artística y técnica que consiste en crear, adquirir, manipular o generar elementos de audio para un medio específico, como el cine, la televisión, los videojuegos"/>
+        <Tarjetadehabilidades titulo="edicion tecnica" descripcion="La edición técnica es el proceso de revisar, corregir y optimizar textos de carácter especializado (científicos, tecnológicos, financieros o académicos) antes de su publicación."/>
+        <Tarjetadehabilidades titulo="Dominio de software de edición"descripcion="Manejo avanzado de programas profesionales como Adobe Premiere Pro, Final Cut Pro o DaVinci Resolve para cortar, estructurar y exportar material visual de forma óptima."/>
+        <Tarjetadehabilidades titulo="Narrativa visual"descripcion="Capacidad para seleccionar las mejores tomas y unirlas con el ritmo, la música y la coherencia necesarios para contar una historia atractiva que mantenga el interés del espectador."/>
+
+    </div>
 </section>

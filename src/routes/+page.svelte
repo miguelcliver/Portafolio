a5b46f72f1cbff2ed55ba2aed770cd96246ecc6f
Miguel Cliver <miguelcliver@gmail.com>
2026-05-22 20:02:58 -0400

    cambio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script>
   import Tarjetadehabilidades from "$lib/components/Tarjetadehabilidades.svelte";
-
 </script>
+
 <section>
   <p>Editor de video profesional</p>
   <div>
     <p>Michael Montañez</p>
-    <p>escrito de algo</p>
+    <p>
+      Hola, soy Michael. Ayudo a empresas a comunicar sus ideas con videos
+      claros, atractivos y de alta calidad. Si buscas un trabajo profesional,
+      puntual y adaptado a tus metas, hagamos equipo en tu próximo proyecto.
+    </p>
   </div>
-  <img src="/unnamed.png" alt="" />
 </section>
 
 <section>
@@ -16,31 +19,52 @@
     <p>Mi historia</p>
   </div>
   <div>
-    <p>texto1</p>
-    <p>texto2</p>
+    <p>
+      Empece en el mundo de edicion por curiosidad y por querer aprender algo
+      nuevo, percatándome de que es algo q me gusta, empecé a realizar trabajos
+      pequeños y ya hace aproximadamente 2 años y 7 meses trabajando
+      profesionalmente.
+    </p>
     <div>
       <div>
-        <p>textoverde1</p>
-        <p>textoblanco1</p>
+        <p>2014-2018</p>
+        <p>inicios en montaje documental y colorimetrica basica</p>
       </div>
       <div>
-        <p>textoverde2</p>
-        <p>textoblanco2</p>
+        <p>2019- presente</p>
+        <p>
+          especialización en postproducción técnica y narrativa para marcas
+          globales
+        </p>
       </div>
     </div>
   </div>
 </section>
 
 <section>
-    <div>
-        <p>HABILIDADES</p>
-    </div>
-    <div>
-        <Tarjetadehabilidades titulo="Soporte tecnico" descripcion="servicio de asistencia que ayuda a los usuarios o empresas a resolver problemas con sus equipos, programas o sistemas tecnológicos" />
-        <Tarjetadehabilidades titulo="diseño sonoro" descripcion="El diseño sonoro es la disciplina artística y técnica que consiste en crear, adquirir, manipular o generar elementos de audio para un medio específico, como el cine, la televisión, los videojuegos"/>
-        <Tarjetadehabilidades titulo="edicion tecnica" descripcion="La edición técnica es el proceso de revisar, corregir y optimizar textos de carácter especializado (científicos, tecnológicos, financieros o académicos) antes de su publicación."/>
-        <Tarjetadehabilidades titulo="Dominio de software de edición"descripcion="Manejo avanzado de programas profesionales como Adobe Premiere Pro, Final Cut Pro o DaVinci Resolve para cortar, estructurar y exportar material visual de forma óptima."/>
-        <Tarjetadehabilidades titulo="Narrativa visual"descripcion="Capacidad para seleccionar las mejores tomas y unirlas con el ritmo, la música y la coherencia necesarios para contar una historia atractiva que mantenga el interés del espectador."/>
-
-    </div>
+  <div>
+    <p>HABILIDADES</p>
+  </div>
+  <div>
+    <Tarjetadehabilidades
+      titulo="Manejo de plataformas"
+      descripcion="Domino las herramientas líderes de la industria audiovisual, con un manejo experto de Adobe Premiere Pro y CapCut. Más allá de conocer el software, optimizo cada flujo de trabajo técnico para garantizar una edición limpia y eficiente adaptada al mercado corporativo."
+    />
+    <Tarjetadehabilidades
+      titulo="Adaptacion a diferentes estilos"
+      descripcion="Cuento con una alta capacidad de adaptación para moldear el estilo de edición según las necesidades de cada proyecto, ya sea contenido corporativo, publicitario o dinámico para redes. Ajusto con precisión el ritmo, la narrativa y la estética visual para asegurar que el video final conecte de forma natural con tu público objetivo."
+    />
+    <Tarjetadehabilidades
+      titulo="Conocimientos de tendencias"
+      descripcion="Me mantengo a la vanguardia de la industria audiovisual, monitoreando constantemente las tendencias y técnicas actuales de edición en redes sociales. Esto me permite aplicar narrativas visuales frescas, ritmos modernos y formatos optimizados que capturan la atención del público de forma inmediata."
+    />
+    <Tarjetadehabilidades
+      titulo="Edicion al detalle"
+      descripcion="Trabajo con un enfoque meticuloso y de alta precisión, revisando minuciosamente cada segundo del metraje para corregir pequeños errores o inconsistencias. Me aseguro de que el color, el audio y los cortes de edición queden perfectamente pulidos para entregar un producto final impecable."
+    />
+    <Tarjetadehabilidades
+      titulo="Gestión del tiempo y cumplimiento de plazos"
+      descripcion="Gestiono cada proyecto con máxima organización, garantizando un flujo de trabajo eficiente que asegura la entrega puntual de tus videos. Mi compromiso con los plazos de entrega te permite planificar tus campañas con total tranquilidad, sin comprometer jamás la calidad final del material.."
+    />
+  </div>
 </section>

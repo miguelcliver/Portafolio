db8d090c5bdd641e111de3fd7fc4e3bfc36486b6
Miguel Cliver <miguelcliver@gmail.com>
2026-05-27 10:34:31 -0400

    icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,10 +27,10 @@
     <p>Todos los derechos reservados</p>
   </div>
   <div>
-    <a href="https://www.instagram.com/?hl=es"><Icon icon="skill-icons:instagram" /></a>
-    <a href="https://vimeo.com/es">vimeo</a>
-    <a href="https://ve.linkedin.com/">linkedin</a>
-  </div>
+    <a href="https://www.instagram.com/?hl=es"><Icon icon="skill-icons:instagram" width={32} height={32}/></a>
+    <a href="https://www.whatsapp.com/?lang=es"><Icon icon="logos:whatsapp-icon" width={32} height={32}/></a>
+    <a href="https://ve.linkedin.com/"><Icon icon="skill-icons:linkedin" width={32} height={32} /></a>
+    </div>
   <div>
     <p>estado disponible</p>
     <p>work@thefame.pro</p>
